f471d7c19705eb3eafad5b29f43a6fb20d623761
yagiraldo62 <yagiraldo62@misena.edu.co>
2020-05-04 16:40:29 -0500

    change manifest

--- src/components/layout/Header/index.js
+++ src/components/layout/Header/index.js
@@ -1,12 +1,13 @@
-import PokeBall from '../../design/PokeBall/mini/';
+// import PokeBall from '../../design/PokeBall/mini/';
 import React from 'react';
 import { pokeball, headerContainer, pokeHeader } from './styles.module.scss';
 import Box from '@material-ui/core/Box';
+import Pokeball from '../../../assets/img//pokeball.svg';
 export default () => (
 	<Box className={pokeHeader}>
 		<Box className={headerContainer}>
 			<h1>Pokedex</h1>
-			<PokeBall loading="true" style={pokeball} />
+			<img src={Pokeball} />
 		</Box>
 	</Box>
 );
